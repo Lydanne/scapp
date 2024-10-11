174ee97a3202c6886ca107767caf8e750bbc2388
Lydanne <1829913225@qq.com>
2024-10-11 23:23:03 +0800

    feat(app): 丰富平台

--- apps/app/src/libs/tapi/platform.ts
+++ apps/app/src/libs/tapi/platform.ts
@@ -1,11 +1,12 @@
 import Taro from '@tarojs/taro';
+import { type } from '@tauri-apps/plugin-os';
 
 export let WEAPP = Taro.getEnv() === Taro.ENV_TYPE.WEAPP;
 export let WEB = Taro.getEnv() !== Taro.ENV_TYPE.WEAPP;
 export let APP = Taro.getEnv() === Taro.ENV_TYPE.WEB && window.__TAURI__;
-export let ANDROID = false;
-export let IOS = false;
-export let QUEST = false;
-export let WINDOWS = false;
-export let LINUX = false;
-export let MAC = false;
+export let ANDROID = APP && type() === 'android';
+export let IOS = APP && type() === 'ios';
+export let QUEST = APP && type() === 'android'; //TODO
+export let WINDOWS = APP && type() === 'windows';
+export let LINUX = APP && type() === 'linux';
+export let MAC = APP && type() === 'macos';
