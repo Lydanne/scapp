8d33bc16ef98346f2efe89c9ee67cbc2f5ce890b
Lydanne <1829913225@qq.com>
2024-10-11 23:08:23 +0800

    refactor: 优化项目框架，更新平台判断逻辑
    优化项目框架，更新平台判断逻辑，使得在WEB环境下，只有在使用Tauri时才将APP标记为true。

--- apps/app/src/libs/tapi/platform.ts
+++ apps/app/src/libs/tapi/platform.ts
@@ -2,7 +2,7 @@ import Taro from '@tarojs/taro';
 
 export let WEAPP = Taro.getEnv() === Taro.ENV_TYPE.WEAPP;
 export let WEB = Taro.getEnv() !== Taro.ENV_TYPE.WEAPP;
-export let APP = Taro.getEnv() === Taro.ENV_TYPE.WEB;
+export let APP = Taro.getEnv() === Taro.ENV_TYPE.WEB && window.__TAURI__;
 export let ANDROID = false;
 export let IOS = false;
 export let QUEST = false;
