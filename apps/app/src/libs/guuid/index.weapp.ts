b8322e77b58db261a868df1a21ab4e2e45c1cd9a
Lydanne <1829913225@qq.com>
2024-11-05 00:20:55 +0800

    fix: 修复细节

--- apps/app/src/libs/guuid/index.weapp.ts
+++ apps/app/src/libs/guuid/index.weapp.ts
@@ -2,7 +2,7 @@ import Taro from '@tarojs/taro';
 
 import config from 'src/config';
 
-import { service } from '../api/core';
+import { request } from '../tapi/request';
 
 /**
  * 获取全局唯一标识符，微信小程序下拿 openid
@@ -10,11 +10,13 @@ import { service } from '../api/core';
 export async function GUUID() {
   const res = await Taro.login();
   const { code } = res;
-  const res2 = await service('POST', '/auth/login', {
+  const res2 = await request({
+    method: 'POST',
+    url: '/auth/login',
     data: {
       appid: config.appId,
       code: code,
     },
   });
-  return res2.openid;
+  return res2.data.openid;
 }
