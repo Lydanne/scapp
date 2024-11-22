7c808529ba4bdd4f2e4535e205fc55085da534a9
刘欢 <1829913225@qq.com>
2024-11-22 19:09:57 +0800

    fix: 添加 base64 完成接受消息

--- apps/app/src-tauri/src/plink/native_channel.rs
+++ apps/app/src-tauri/src/plink/native_channel.rs
@@ -1,15 +1,16 @@
 use once_cell::sync::Lazy;
-use protobuf::Message;
+use protobuf::{EnumOrUnknown, Message};
 use rand::Rng;
 use serde::Serialize;
 use tauri::Emitter;
 use tauri::{ipc::Channel, AppHandle};
 use std::collections::HashMap;
+use std::net::SocketAddr;
 use std::sync::{Arc, Mutex, RwLock};
 use tokio::net::UdpSocket;
 use tokio::sync::mpsc;
 
-use crate::native_connection::ChannelStatus;
+use crate::native_connection::{ChannelStatus, OnDataStatus, PipeData, SynReadySignalPipe};
 use crate::send_packet;
 use crate::{proto::payload, receive_packet, Udp};
 
@@ -68,6 +69,7 @@ pub async fn native_channel_listen(app: AppHandle) -> u32 {
                             // println!("connections {:?}", connections);
 
                             let client = connections.get_mut(&channel.id);
+
                             match action {
                                 payload::channel::Action::Connect(data) => {
                                     println!("connect");
@@ -98,10 +100,7 @@ pub async fn native_channel_listen(app: AppHandle) -> u32 {
                                                     ),
                                                     special_fields: Default::default(),
                                                 };
-                                                let socket_sub = socket.clone();
-                                                tokio::spawn(async move {
-                                                    send_packet(socket_sub, &on_received.remote_info, message.write_to_bytes().unwrap().to_vec()).await;
-                                                });
+                                                send_message(socket.clone(), &on_received.remote_info, message);
                                             }
                                             client.status = ChannelStatus::Connected;
                                             app.emit("em_connection", client.clone()).unwrap();
@@ -110,20 +109,13 @@ pub async fn native_channel_listen(app: AppHandle) -> u32 {
                                             let message = payload::Channel {
                                                 version: 1,
                                                 id: channel.id,
-                                                ts: std::time::SystemTime::now()
-                                                    .duration_since(std::time::UNIX_EPOCH)
-                                                    .unwrap()
-                                                    .as_millis()
-                                                    as u64,
+                                                ts: get_ts(),
                                                 action: Some(payload::channel::Action::Disconnect(
                                                     payload::DisconnectAction::new(),
                                                 )),
                                                 special_fields: Default::default(),
                                             };
-                                            let socket_sub = socket.clone();
-                                            tokio::spawn(async move {
-                                                send_packet(socket_sub, &on_received.remote_info, message.write_to_bytes().unwrap().to_vec()).await;
-                                            });
+                                            send_message(socket.clone(), &on_received.remote_info, message);
                                         }
                                     } else {
                                         println!("connect client not found");
@@ -138,11 +130,7 @@ pub async fn native_channel_listen(app: AppHandle) -> u32 {
                                         let message = payload::Channel {
                                             version: 1,
                                             id: channel.id,
-                                            ts: std::time::SystemTime::now()
-                                                .duration_since(std::time::UNIX_EPOCH)
-                                                .unwrap()
-                                                .as_millis()
-                                                as u64,
+                                            ts: get_ts(),
                                             action: Some(payload::channel::Action::Connect(
                                                 payload::ConnectAction {
                                                     seq: seq,
@@ -152,24 +140,96 @@ pub async fn native_channel_listen(app: AppHandle) -> u32 {
                                             )),
                                             special_fields: Default::default(),
                                         };
-                                        let socket_sub = socket.clone();
-                                        tokio::spawn(async move {
-                                            send_packet(socket_sub, &on_received.remote_info, message.write_to_bytes().unwrap().to_vec()).await;
-                                        });
+                                        send_message(socket.clone(), &on_received.remote_info, message);
                                     }
                                 }
                                 payload::channel::Action::Disconnect(_) => {
                                     println!("disconnect");
                                 }
-                                payload::channel::Action::Data(_) => {
+                                payload::channel::Action::Data(action) => {
                                     println!("data");
-                                    let mut connections = CONNECTIONS.lock().unwrap();
-                                    if let Some(client) = connections.get_mut(&channel.id) {
-                                        app.emit("em_data", channel.write_to_bytes().unwrap()).unwrap();
+                                    if let Some(client) = client {
+                                        // client.lasts.push(on_received.message.to_vec());
+
+                                        let pipe = client.pipe_map.get_mut(&action.id).unwrap();
+
+                                        pipe.buffers.push(action.body.clone());
+                                        pipe.received += 1;
+                                        pipe.received_bytes += action.body.len() as u32;
+
+                                        let message = payload::Channel {
+                                            version: 1,
+                                            id: channel.id,
+                                            ts: get_ts(),
+                                            action: Some(payload::channel::Action::Sync(
+                                                payload::SyncAction {
+                                                    id: action.id,
+                                                    signal: Some(payload::sync_action::Signal::AckChunkFinish(
+                                                        payload::AckChunkFinish {
+                                                            index: action.index,
+                                                            status: EnumOrUnknown::new(payload::FinishStatus::Ok),
+                                                            special_fields: Default::default(),
+                                                        },
+                                                    )),
+                                                    special_fields: Default::default(),
+                                                },
+                                            )),
+                                            special_fields: Default::default(),
+                                        };
+                                        send_message(socket.clone(), &on_received.remote_info, message);
+
+                                        if pipe.received == pipe.head.length {
+                                            pipe.status = OnDataStatus::Done;
+                                            pipe.progress = 100;
+                                            pipe.speed = pipe.received_bytes as u32 / (get_ts() - pipe.start_time) as u32;
+                                            app.emit("em_data", pipe.to_owned()).unwrap();
+                                        }
                                     }
                                 }
-                                payload::channel::Action::Sync(_) => {
+                                payload::channel::Action::Sync(action) => {
                                     println!("sync");
+                                    if let Some(client) = client {
+                                        if let Some(payload::sync_action::Signal::SynReady(ready)) = &action.signal {
+                                            send_message(socket.clone(), &on_received.remote_info, payload::Channel {
+                                                version: 1,
+                                                id: channel.id,
+                                                ts: get_ts(),
+                                                action: Some(payload::channel::Action::Sync(
+                                                    payload::SyncAction {   
+                                                        id: action.id,
+                                                        signal: Some(payload::sync_action::Signal::AckReady(
+                                                            payload::AckReadySignal {
+                                                                length: ready.length,
+                                                                size: ready.size,
+                                                                sign: ready.sign.clone(),
+                                                                special_fields: Default::default(),
+                                                            },
+                                                        )),
+                                                        special_fields: Default::default(),
+                                                    },
+                                                )),
+                                                special_fields: Default::default(),
+                                            });
+                                            client.pipe_map.insert(action.id, PipeData{
+                                                id: action.id,
+                                                index: 0,
+                                                status: OnDataStatus::Ready,
+                                                tp: ready.type_.into(),
+                                                progress: 0,
+                                                speed: 0,
+                                                head: SynReadySignalPipe{
+                                                    length: ready.length,
+                                                    size: ready.size,
+                                                    sign: ready.sign.clone(),
+                                                    name: ready.name.clone(),
+                                                },
+                                                buffers: vec![],
+                                                received: 0,
+                                                received_bytes: 0,
+                                                start_time: channel.ts,
+                                            });
+                                        }
+                                    }
                                 }
                                 payload::channel::Action::Detect(_) => {
                                     println!("detect");
@@ -196,4 +256,18 @@ pub async fn native_channel_close() -> bool {
 pub fn rand_u32() -> u32 {
     rand::thread_rng().gen_range(1..100)
 }
-    
+
+pub fn send_message(socket: Arc<UdpSocket>, remote_info: &SocketAddr, message: payload::Channel) {
+    let remote = remote_info.clone();
+    let message_bytes = message.write_to_bytes().unwrap().to_vec();
+    tokio::spawn(async move {
+        send_packet(socket, &remote, message_bytes).await;
+    });
+}
+
+pub fn get_ts() -> u64 {
+    std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as u64
+}
--- apps/app/src-tauri/src/plink/native_connection.rs
+++ apps/app/src-tauri/src/plink/native_connection.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
+
+use protobuf::EnumOrUnknown;
 use serde::Serialize;
 
-use super::proto::payload::{DataType, SynReadySignal};
+use super::proto::payload::{self, DataType, SynReadySignal};
 
 #[derive(PartialEq, Debug, Clone, Serialize)]
 #[serde(into = "i32")]
@@ -18,6 +21,81 @@ impl Into<i32> for ChannelStatus {
     }
 }
 
+#[derive(Clone, Serialize, Debug, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct SynReadySignalPipe {
+    pub length: u32,
+    pub size: u32,
+    pub sign: String,
+    pub name: String,
+}
+
+
+impl From<SynReadySignal> for SynReadySignalPipe {
+    fn from(value: SynReadySignal) -> Self {
+        Self {
+            length: value.length,
+            size: value.size,
+            sign: value.sign,
+            name: value.name,
+        }
+    }
+}
+
+#[derive(Clone, Serialize, Debug)]
+#[serde(into = "i32")]
+pub enum DataTypePipe {
+    TEXT = 0,
+    FILE = 1,
+}
+
+impl Into<i32> for DataTypePipe {
+    fn into(self) -> i32 {
+        self as i32
+    }
+}
+
+impl From<DataType> for DataTypePipe {
+    fn from(value: DataType) -> Self {
+        match value {
+            DataType::TEXT => DataTypePipe::TEXT,
+            DataType::FILE => DataTypePipe::FILE,
+        }
+    }
+}
+
+impl From<i32> for DataTypePipe {
+    fn from(value: i32) -> Self {
+        match value {
+            0 => DataTypePipe::TEXT,
+            1 => DataTypePipe::FILE,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl From<EnumOrUnknown<DataType>> for DataTypePipe {
+    fn from(value: EnumOrUnknown<DataType>) -> Self {
+        value.value().into()
+    }
+}
+
+#[derive(Clone, Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct PipeData {
+    pub id: u32,
+    pub index: u32,
+    pub status: OnDataStatus,
+    pub tp: DataTypePipe,
+    pub progress: u32,
+    pub speed: u32,
+    pub head: SynReadySignalPipe,
+    pub buffers: Vec<Vec<u8>>,
+    pub received: u32,
+    pub received_bytes: u32,
+    pub start_time: u64,
+}
+
 #[derive(Clone, Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct NativeConnection {
@@ -25,15 +103,19 @@ pub struct NativeConnection {
     pub status: ChannelStatus,
     pub socket_ip: String,
     pub seq: u32,
+    pub lasts: Vec<Vec<u8>>,
+    pub pipe_map: HashMap<u32, PipeData>,
 }
 
 impl NativeConnection {
     pub fn new(id: u32, socket_ip: String) -> Self {
         Self {
             id,
-            status: ChannelStatus::Init,
+            status: ChannelStatus::Disconnected,
             socket_ip,
             seq: 0,
+            lasts: Vec::new(),
+            pipe_map: HashMap::new(),
         }
     }
 
@@ -46,10 +128,10 @@ impl NativeConnection {
             id: self.id,
             index: self.seq,
             status: OnDataStatus::Ready,
-            tp: DataType::TEXT,
+            tp: DataTypePipe::TEXT,
             progress: 0,
             speed: 0,
-            head: SynReadySignal::new(),
+            head: SynReadySignalPipe::default(),
             body: "".to_string(),
         });
     }
@@ -57,24 +139,52 @@ impl NativeConnection {
 
 pub struct SendData {
     pub id: u32,
-    pub tp: DataType,
-    pub head: SynReadySignal,
+    pub tp: DataTypePipe,
+    pub head: SynReadySignalPipe,
     pub body: String,
 }
 
+#[derive(Clone, Serialize, Debug)]
+#[serde(into = "i32")]
 pub enum OnDataStatus {
-    Ready,
-    Sending,
-    Done,
+    Ready = 0,
+    Sending = 1,
+    Done = 2,
 }
 
+impl Into<i32> for OnDataStatus {
+    fn into(self) -> i32 {
+        self as i32
+    }
+}
+
+#[derive(Clone, Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct OnData {
     pub id: u32,
     pub index: u32,
     pub status: OnDataStatus,
-    pub tp: DataType,
+    pub tp: DataTypePipe,
     pub progress: u32,
     pub speed: u32,
-    pub head: SynReadySignal,
+    pub head: SynReadySignalPipe,
     pub body: String,
+}
+
+impl From<PipeData> for OnData {
+    fn from(value: PipeData) -> Self {
+        let body = value.buffers.into_iter().flatten().collect::<Vec<u8>>();
+        let body = base64::decode(&body).unwrap_or(body);
+        let body = String::from_utf8(body).unwrap_or_default();
+        Self {
+            id: value.id,
+            index: value.index,
+            status: value.status,
+            tp: value.tp,
+            progress: value.progress,
+            speed: value.speed,
+            head: value.head,
+            body,
+        }
+    }
 }
